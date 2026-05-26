28d35bbee7ee1ff86ece97b048950236019c7a35
Maxim Pass <4Max0@proton.me>
2026-05-26 19:33:54 +0200

    Regex highlighter Test hinzugefuegt

---
+++ src/test/java/highlighting/regex/RegexHighlighterTest.java
@@ -0,0 +1,12 @@
+package highlighting.regex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RegexHighlighterTest {
+  @Test
+  void Test() {
+    assertTrue(true);
+  }
+}
